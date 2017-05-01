b92089d97abe3373729db8fd077f0c76181e085f
Patrick Herbert <patricktrherbert@gmail.com>
2017-05-01 10:35:59 -0700

    Integrate Kent's Ray Collision PT. 2
    Camera.h was unselected during the last commit

--- 3DPacman/Camera.h
+++ 3DPacman/Camera.h
@@ -2,11 +2,11 @@
 #define CAMERA_H
 
 //////////////////////////////////////////////////////////////////////////////////////////////
-// Purpose: Returns true if there is a collision between where the camera is going and any wall
+// Purpose: Returns true if there is a collision between where the camera is going and any wall (GRID system)
 // Input: (int) direction - the direction the camera is moving, case 0 = forwards, case 1 = reverse
 // Author: Patrick H. 4/17
 //////////////////////////////////////////////////////////////////////////////////////////////
-bool cameraIsCollidingGrid(int direction) {
+bool cameraIsCollidingWallGrid(int direction) {
 	float posX = 0.0;
 	float posZ = 0.0;
 	bool isCollision = false;
@@ -60,14 +60,46 @@ bool cameraIsCollidingGrid(int direction) {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
-// Purpose: Based on status of W/S/A/D keys, move the camera forward, in reverse or not at all,
-//  (cont): Called by display()
+// Purpose: Returns true if there is a collision between where the camera is going and any wall (RAY intersection)
 // Author: Patrick H. 4/17
 //////////////////////////////////////////////////////////////////////////////////////////////
-void moveCamera() {
+bool cameraIsCollidingWallRay() {
+	float posX = 0.0;
+	float posZ = 0.0;
+	bool isCollision = false;
+	float size = 2.0;
+
+	for (int i = 0; i < maze.size(); i++) {
+		if (maze[i] == '#') {
+			if ((eye.x > posX - size) && (eye.x < posX + size) && (eye.z > posZ - size) && (eye.z < posZ + size)) {
+				if (wall.collision(theta, vec3(posX, 0.0, posZ))) {
+					isCollision = true;
+					break;
+				}
+			}
+
+			posX += 2.0;
+		}
+		else if (maze[i] == 'E') {
+			posX = 0.0;
+			posZ += 2.0;
+		}
+		else {
+			posX += 2.0;
+		}
+	}
+
+	return isCollision;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Purpose: Moves camera forwards || backwards || strafeleft || straferight, if there is no collision (GRID system)
+// Author: Patrick H. 4/17
+//////////////////////////////////////////////////////////////////////////////////////////////
+void gridCollision() {
 	if (goForward == true)
 	{
-		if (!cameraIsCollidingGrid(0)) {
+		if (!cameraIsCollidingWallGrid(0)) {
 			eye = eye + cameraSpeed*view;
 			at = at + cameraSpeed*view;
 		}
@@ -75,7 +107,7 @@ void moveCamera() {
 
 	if (goReverse == true)
 	{
-		if (!cameraIsCollidingGrid(1)) {
+		if (!cameraIsCollidingWallGrid(1)) {
 			eye = eye - cameraSpeed*view;
 			at = at - cameraSpeed*view;
 		}
@@ -86,9 +118,49 @@ void moveCamera() {
 		//...
 	}
 
-	if (strafeRight == true) {
+	if (strafeRight == true)
+	{
 		//...
 	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Purpose: Moves camera forwards || backwards || strafeleft || straferight, if there is no collision (RAY intersection)
+// Author: Patrick H. 4/17
+//////////////////////////////////////////////////////////////////////////////////////////////
+void rayCollision() {
+	if (!cameraIsCollidingWallRay()) {
+		if (goForward == true)
+		{
+			eye = eye + cameraSpeed*view;
+			at = at + cameraSpeed*view;
+		}
+
+		if (goReverse == true)
+		{
+			eye = eye - cameraSpeed*view;
+			at = at - cameraSpeed*view;
+		}
+
+		if (strafeLeft == true)
+		{
+			//...
+		}
+
+		if (strafeRight == true) {
+			//...
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Purpose: Based on status of W/S/A/D keys, move the camera forward, in reverse or not at all,
+//  (cont): Called by display()
+// Author: Patrick H. 4/17
+//////////////////////////////////////////////////////////////////////////////////////////////
+void moveCamera() {
+	//rayCollision();
+	gridCollision();
 }
 
 #endif
